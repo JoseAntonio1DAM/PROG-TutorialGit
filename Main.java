fda33426a48841c051937e7cd0a39fba8ca74b1f
José Antonio Herrera Expósito <joseantonioherreraexposito@gmail.com>
2025-04-29 10:58:34 +0100

    feat(Main.java): ejercico 1.3 clase apuntes PRO

--- Main.java
+++ Main.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hola mundo y los demás!");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Dame un número:");
+        int number = sc.nextInt();
+        System.out.println("El número es:" + number);
+
+        sc.close();
     }
 }
